2d4a00f15373d4df672ccdc81a6bf4bf9f4e485e
Kent Zamudio <kazamudio@up.edu.ph>
2020-12-28 17:42:59 +0800

    modified elem.hs

--- src/elem.hs
+++ src/elem.hs
@@ -1,10 +1,5 @@
---faulty program that checks if element e is in list l
+--program that checks if element e is in list
     
--- elem :: (Eq a) => [a] -> a -> Bool
+elem :: (Eq a) => [a] -> a -> Bool
 iselem e (x:xs) = (e==x) || (iselem e xs)
 iselem e [] = False
-
--- main = do
---     let l = [1..5]
---     print (elem 5 l)
---     -- print (elem l 8)
