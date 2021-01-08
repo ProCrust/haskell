30a21854eff2f4dfcf57b4136ecc99e732ac8b42
Kent Zamudio <kazamudio@up.edu.ph>
2021-01-09 01:25:31 +0800

    modified elem.hs

--- src/elem.hs
+++ src/elem.hs
@@ -1,5 +1,5 @@
 --program that checks if element e is in list
     
-elem :: (Eq a) => [a] -> a -> Bool
+-- iselem :: (Eq a) => [a] -> a -> Bool
 iselem e (x:xs) = (e==x) || (iselem e xs)
 iselem e [] = False
